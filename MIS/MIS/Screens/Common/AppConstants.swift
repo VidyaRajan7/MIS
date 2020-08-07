0b3acec306c6ab769895248a62c7d19fc9e8211a
Developer Admin <developeradmin@Developers-MacBook-Air.local>
2020-08-07 12:14:18 +0530

    Added App constants

---
+++ MIS/MIS/Screens/Common/AppConstants.swift
@@ -0,0 +1,37 @@
+//
+//  AppConstants.swift
+//  MIS
+//
+//  Created by Developer Admin on 07/08/20.
+//  Copyright © 2020 Developer Admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct StoryboardIdentifier {
+    static let mainStoryBoard = "Main"
+    static let landingViewController = "LandingViewController"
+}
+
+struct CommonStrings {
+    static let loginName = "Login"
+    static let emptyString = ""
+}
+
+struct FontName {
+    static let gothamLight = "Gotham Light"
+}
+
+struct DeviceType {
+    static let IS_IPHONE_5_OR_LESS = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH <= 568.0
+    static let IS_IPAD = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
+    static let IS_IPAD_PRO = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1366.0
+}
+
+struct ScreenSize {
+    static let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+    static let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
+    static let SCREEN_MAX_LENGTH = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+    static let SCREEN_MIN_LENGTH = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+}
---
+++ MIS/MIS/Screens/LandingViewController/Controller/LandingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingViewController.swift
+//  MIS
+//
+//  Created by Developer Admin on 06/08/20.
+//  Copyright © 2020 Developer Admin. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
